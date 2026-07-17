dfb7758dc0ad901c09e0544df9d1e4d3836c169d
Rodrigo Lessa <rodrigo.lessa@senior.com.br>
2026-07-17 17:07:45 -0300

    Auto commit on save

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 
-{ }
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
